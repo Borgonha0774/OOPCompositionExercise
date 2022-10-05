245210e077b7f5a0cc874805e51e16f497976a86
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-05 09:12:09 -0300

    add enum order status

---
+++ OOPCompositionExercise/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace OOPCompositionExercise.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+
+    }
+}
