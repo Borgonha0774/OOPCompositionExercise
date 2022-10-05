1933fc10a476a20ff89ddf8e170741ea57c5fcd8
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-05 09:21:37 -0300

    create class client

---
+++ OOPCompositionExercise/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace OOPCompositionExercise.Entities
+{
+    internal class Client
+    {
+        public string  Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirtDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birtDate)
+        {
+            Name = name;
+            Email = email;
+            BirtDate = birtDate;
+        }
+    }
+}
